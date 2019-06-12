7360d9adde9be3ec8ba6f0c7b49f79c10190e82a
peter <peter1996116@gmail.com>
2019-06-12 13:21:49 +0800

    Initial Commit

---
+++ pageAndDelegate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pageAndDelegate
+//
+//  Created by 金陳廷 on 2019/6/12.
+//  Copyright © 2019 notu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
